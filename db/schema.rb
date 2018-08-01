4049efde51e3936d9b826caa18fe330d697d79cc
github username <github email address>
2018-07-31 23:17:03 -0400

    drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_022347) do
+ActiveRecord::Schema.define(version: 2018_08_01_031623) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 2018_08_01_022347) do
     t.integer "workout_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "first_name"
-    t.string "last_name"
   end
 
   create_table "workouts", force: :cascade do |t|
